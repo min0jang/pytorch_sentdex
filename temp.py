75852590c703b9aa21e819ced1e48f8b6acfa641
USER <user@AL01414017.local>
2020-05-06 14:01:15 +0900

    added training and validation

--- temp.py
+++ temp.py
@@ -3,6 +3,7 @@ from torchvision import transforms, datasets
 import matplotlib.pyplot as plt
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.optim as optim
 
 train = datasets.MNIST("", train=True,
                            download=True,
@@ -37,7 +38,44 @@ class Net(nn.Module):
 
 net = Net()
 
-X = torch.rand((28, 28))
-output = net(X.view(1,28*28))
+'''
+Training
+'''
 
-print("It has been pushed correctly")
+optimizer = optim.Adam(net.parameters(), lr=0.001)
+EPOCHS = 1
+
+for epoch in range(EPOCHS):
+    print(f"Epoch Number: {epoch}")
+    for data in trainset:
+        # data is a batach of featuresets and labels
+        X, y = data
+        net.zero_grad()
+        output = net(X.view(-1, 28*28))
+        loss = F.nll_loss(output, y)    # nll_loss for one-hot, mean-squared for vectors
+        loss.backward()     # back propagation
+        optimizer.step()
+    
+    print(loss)
+
+
+'''
+Validation
+'''
+correct = 0
+total = 0
+
+with torch.no_grad():
+    for data in trainset:
+        output = net(X.view(-1, 28*28))
+
+        for idx, i in enumerate(output):
+            if torch.argmax(i) == y[idx]:
+                correct += 1
+            total += 1
+
+print("Accuracy: ", round(correct/total, 3))
+
+import matplotlib.pyplot as plt
+plt.imshow(X[0].view(28,28))
+plt.show()
